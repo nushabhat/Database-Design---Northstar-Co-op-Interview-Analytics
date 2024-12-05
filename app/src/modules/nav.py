6c369cce8a3f94dc909595f93f33299aa9f5a6f1
farciethica <qalewine@gmail.com>
2024-12-05 13:12:54 -0500

    removed sidebar ML for student submission code

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -77,9 +77,7 @@ def ClassificationNav():
 #### ------------------------ System Admin Role ------------------------
 def AdminPageNav():
     st.sidebar.page_link("pages/20_Admin_Home.py", label="System Admin", icon="🖥️")
-    st.sidebar.page_link(
-        "pages/21_ML_Model_Mgmt.py", label="ML Model Management", icon="🏢"
-    )
+
     st.sidebar.page_link(
         "pages/22_Survey_Questions_Mgmt.py", label="Manage Survey Questions", icon="📋"
     )
